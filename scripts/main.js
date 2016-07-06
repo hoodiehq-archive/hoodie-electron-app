5312ae27c9ac39bf6561c5700a3403e6e5da38ca
Gregor <gregor@martynus.net>
2016-07-06 15:27:34 -0400

    Merge code from Rookies-RGSOC2016/Hoodie_Desktop_app-2016
    Merges code from https://github.com/Rookies-RGSOC2016/Hoodie_Desktop_app-2016
    
    for next steps, I would suggest we do
    
    1. rename `Hoodie-electron-demo` to `hoodie-electron-app` (we usually use lowercase letters only in repository names, because it uppercase letters may cause issues between Windows / Mac / Linux
    2. create `gh-pages` branch from `master` and make it the main branch
    3. Delete https://github.com/Rookies-RGSOC2016/Hoodie_Desktop_app-2016
    
    Let me know if you have any questions :)

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+$(function() {
+	//change app name
+	$('#js-change-appname').on('click', function () {
+		var changed = $('#rename-app').val();
+		$('#name-app').text( changed );
+	});
+
+	//toggle start/stop button
+	$('#main-button').on('click', function () {
+		var $el = $(this),
+		textNode = this.lastChild;
+
+		$el.find('span').toggleClass('glyphicon-play glyphicon-stop');
+		textNode.nodeValue = ($el.hasClass('main-button') ? 'Stop' : 'Start')
+		$el.toggleClass('main-button');
+	});
+
+});
+
+var el = document.querySelector("#create-app-btn");
+if (el) {
+  el.addEventListener("click", function(){
+    document.querySelector("#create-app").style.display = "block";
+  });
+}
+
+$(function myFunction() {
+    var $myDropdown = document.getElementById("myDropdown")
+		if ($myDropdown) {
+			$myDropdown.classList.toggle("show");
+		}
+})
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}

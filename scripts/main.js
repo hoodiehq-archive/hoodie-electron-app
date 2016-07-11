a0b675129444b9f9698c81d7571ab7e032a20d6f
Hyesoo Park <flyjwayur@gmail.com>
2016-07-11 15:20:19 +0300

    saved new app in the local storage, redirect to the detail_app- get the value

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,53 @@
+//index.html
+//display the creat app element, when a user click the new app button
+var newAppBtn = document.querySelector("#new-app-btn");
+if (newAppBtn) {
+	newAppBtn.addEventListener("click", function(){
+		document.querySelector("#create-app").style.display = "block";
+		//create an new app, when a user click 'create button'
+		var createBtn = document.querySelector("#choose-create");
+		debugger;
+		if(createBtn){
+			createBtn.addEventListener("click",function(){
+				var appname = document.getElementsByTagName("input")[0].value;
+				console.log('appname_value: '+ appname);
+				//save the appname to the local storage
+				if(appname){
+					console.log('appname_localstorage: '+ appname);
+					localStorage.setItem("app-name", appname);
+					//redirect from index page to detail_app_page
+					//JS
+					window.location.replace("detail_app_page.html");
+					//Jquery
+					// $(document).ready(function(){
+					// 	var url = "detail_app_page.html";
+					// 	$(location).attr('href',url);
+					// });
+				}
+			});
+		}
+	});
+}
+
+//empty the text in the text field, when a user click the 'cancel button'
+var cancelBtn = document.querySelector("#cancel-create");
+if(cancelBtn){
+	cancelBtn.addEventListener("click",function(){
+		$('#empty-text').val('');
+	});
+}
+
+//detail_app_page.html
+var getAppName = localStorage.getItem("app-name");
+console.log(getAppName);
+if(getAppName){
+	console.log("if getName"+getAppName);
+	document.getElementById('#name-app').innerHTML = getAppName;
+	//$("#name-app").innerHTML = getAppName;
+}
+
+
+
 $(function() {
 	//change app name
 	$('#js-change-appname').on('click', function () {
@@ -17,31 +67,25 @@ $(function() {
 
 });
 
-var el = document.querySelector("#create-app-btn");
-if (el) {
-  el.addEventListener("click", function(){
-    document.querySelector("#create-app").style.display = "block";
-  });
-}
-
-$(function myFunction() {
-    var $myDropdown = document.getElementById("myDropdown")
-		if ($myDropdown) {
-			$myDropdown.classList.toggle("show");
-		}
-})
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// //sidebar
+// $(function myFunction() {
+//     var $myDropdown = document.getElementById("myDropdown")
+// 		if ($myDropdown) {
+// 			$myDropdown.classList.toggle("show");
+// 		}
+// })
+//
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }

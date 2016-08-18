9a4674cc6666e750e13be41d4d9a50f3b814365a
Gregor <gregor@martynus.net>
2016-08-18 12:39:52 -0700

    style: unify indentation, no code changes

--- scripts/indexApi.js
+++ scripts/indexApi.js
@@ -1,28 +1,26 @@
 var applist = {}
 
-applist.findAll = function () {
+applist.findAll = function() {
   var appsData = localStorage.getItem('apps');
   var apps = appsData ? JSON.parse(appsData) : [];
   return Promise.resolve(apps)
 }
 
-applist.add = function (app){
+applist.add = function(app) {
   app.id = Math.random().toString(36).substr(2, 7);
   return applist.findAll()
-  .then (function(apps){
-    apps.push(app)
-    localStorage.setItem('apps', JSON.stringify(apps))
-    return app
-  })
+    .then(function(apps) {
+      apps.push(app)
+      localStorage.setItem('apps', JSON.stringify(apps))
+      return app
+    })
 }
-applist.find = function(id){
-return applist.findAll ()
-.then(function(apps){
-  var findApp = function(app) {
-    return app.id === id;
-  };
-  return apps.find(findApp);
-
-})
-
+applist.find = function(id) {
+  return applist.findAll()
+    .then(function(apps) {
+      var findApp = function(app) {
+        return app.id === id;
+      };
+      return apps.find(findApp);
+    })
 }
